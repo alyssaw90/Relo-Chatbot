f8ba0db88c0b337b1a43b19a85682dfb757ad495
Norton Pengra <npengra317@gmail.com>
2016-07-14 15:56:20 -0700

    Create Intent directory for master bot

---
+++ ReloChat/ReloChatBot/Models/IntentDirectory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReloChatBot.Models
+{
+    public static class IntentDirectory
+    {
+        public static Dictionary<string, string> actions = new Dictionary<string, string>()
+        {
+            { "RedirectTransportation", "Sounds like you have commute questions" },
+            { "RedirectLodging", "Sounds like you have lodging questions" },
+            { "WhatIsLeap", "Sounds like you have questions regarding what LEAP is" },
+            { "WhereIsLeap", "Leap is located in Microsoft Building 86." },
+            { "None", "I'm not sure if I understood you properly." }
+        };
+    }
+}
