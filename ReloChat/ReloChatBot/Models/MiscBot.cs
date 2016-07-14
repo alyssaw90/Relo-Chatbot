4b1a54cb6106eef2e967b4f64d83e5bb4f5cf80b
Jennyf19 <ferriesj@gmail.com>
2016-07-14 15:16:03 -0700

    create LuisParser for MiscBot

---
+++ ReloChat/ReloChatBot/Models/MiscBot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ReloChatBot;
+
+namespace ReloChatBot.Models
+{
+    public class MiscBot
+    {
+        private LuisClient client;
+        private string raw_result;
+
+        private string api_endpoint =
+            "https://api.projectoxford.ai/luis/v1/application?id=024edd6d-8598-4a3c-b942-05705dfd39b7&subscription-key=ba3ed0b23b714c4f9a8ca0fe2eeddcea&q=";
+
+        public MiscBot(string query)
+        {
+            this.client = new LuisClient();
+            this.raw_result = this.client.QueryLuis(this.api_endpoint, query);
+        }
+    }
+}
+
+using RelochatBot.models;
+var x = new MiscBot("hello world");
+x.raw_result;
