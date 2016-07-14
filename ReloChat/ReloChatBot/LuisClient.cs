df82f5515e4297bb018c002022d8c8e674eec599
Mary <marysam01@hotmail.com>
2016-07-14 00:25:39 -0700

    Added LuisInfo and LuisClient. Added folders for Commute.

---
+++ ReloChat/ReloChatBot/LuisClient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace ReloChatBot
+{
+    public class LuisClient
+    {
+        //A method that will send the user's input as a query to Luis and returns the intents and entities.
+        public static async Task<LuisInfo> QueryLuis(string strInput)
+        {
+            string strRet = string.Empty;
+
+            //EscapeDataString removes invalid Uri characters i.e. spaces.
+            string strEscaped = Uri.EscapeDataString(strInput);
+
+            using (var client = new HttpClient())
+            {
+                //The Luis URI will be inserted below.
+                string uri = "Paste the Master Luis URI Here&q=" + strEscaped;
+                HttpResponseMessage msg = await client.GetAsync(uri);
+
+                if (msg.IsSuccessStatusCode)
+                {
+                    string jsonResponse = await msg.Content.ReadAsStringAsync();
+                    LuisInfo _Data = JsonConvert.DeserializeObject<LuisInfo>(jsonResponse);
+                    return _Data;
+                }
+            }
+            return null;
+        }
+    }
+}
