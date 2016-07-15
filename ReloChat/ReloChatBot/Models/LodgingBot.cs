2cfea0dd80b85e581e42981247b3633fdf70b661
Norton Pengra <npengra317@gmail.com>
2016-07-15 14:56:23 -0700

    Remove Redirection Required

--- ReloChat/ReloChatBot/Models/LodgingBot.cs
+++ ReloChat/ReloChatBot/Models/LodgingBot.cs
@@ -11,6 +11,7 @@ namespace ReloChatBot.Models
         protected string api_endpoint = "https://api.projectoxford.ai/luis/v1/application?id=8547ee95-9496-43ca-a9a2-b583da92cd7e&subscription-key=6171c439d26540d6a380208a16b31958?q=";
         public LodgingBot(string query) : base(query) { }
     }
+
 }
 
 
