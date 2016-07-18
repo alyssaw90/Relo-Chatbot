d00bc8418eeed90df1c2b635b7520b6199506f1c
Norton Pengra <npengra317@gmail.com>
2016-07-18 11:13:58 -0700

    New BotController for easier API access

---
+++ ReloChat/ReloChatBot/Controllers/BotController.cs
@@ -0,0 +1,49 @@
+﻿using ReloChatBot.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReloChatBot.Controllers
+{
+    public class BotController
+    {
+        const string RedirectLodging = "RedirectLodging";
+
+        private LuisParser masterbot;
+        private string userinput;
+
+        private string reply;
+
+        public BotController(LuisParser masterbot, string userinput)
+        {
+            this.masterbot = masterbot;
+            this.userinput = userinput;
+            if (masterbot.RedirectRequired)
+            {
+                if (masterbot.Intent == RedirectLodging)
+                {
+                    this.handle_RedirectLodging();
+                }
+                else
+                {
+                    throw new Exception("No handle created for \"" + this.masterbot.Intent + "\"!");
+                }
+            } else
+            {
+                this.reply = "";
+            }
+        }
+        private void handle_RedirectLodging()
+        {
+            LodgingBot lobot = new LodgingBot(this.userinput);
+            this.reply = lobot.Reply;
+        }
+
+        public string Reply
+        {
+            get { return this.reply; }
+        };
+
+    }
+}
