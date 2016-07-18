79db54fa6328c7eb5e8283daf0c77d5a3d938ca7
Norton Pengra <npengra317@gmail.com>
2016-07-18 09:30:38 -0700

    Fix syntax errors and create new Method

--- ReloChat/ReloChatBot/Models/LodgingBot.cs
+++ ReloChat/ReloChatBot/Models/LodgingBot.cs
@@ -8,8 +8,18 @@ namespace ReloChatBot.Models
     public class LodgingBot : LuisParser
     {
         // Spot the python programmer again...
-        protected string api_endpoint = "https://api.projectoxford.ai/luis/v1/application?id=8547ee95-9496-43ca-a9a2-b583da92cd7e&subscription-key=6171c439d26540d6a380208a16b31958?q=";
+        protected new string api_endpoint = "https://api.projectoxford.ai/luis/v1/application?id=8547ee95-9496-43ca-a9a2-b583da92cd7e&subscription-key=6171c439d26540d6a380208a16b31958?q=";
         public LodgingBot(string query) : base(query) { }
+        public new string Reply
+        {
+            get { return this.GetReply(); }
+        }
+
+        private string GetReply()
+        {
+
+            return this.Intent;
+        }
     }
 
 }
