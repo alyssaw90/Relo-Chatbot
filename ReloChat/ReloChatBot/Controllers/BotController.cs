745d63a847029956831a67254643b84c5c1b1979
Norton Pengra <npengra317@gmail.com>
2016-07-21 10:25:06 -0700

    Create handle_RedirectMiscBot

--- ReloChat/ReloChatBot/Controllers/BotController.cs
+++ ReloChat/ReloChatBot/Controllers/BotController.cs
@@ -10,6 +10,7 @@ namespace ReloChatBot.Controllers
     public class BotController
     {
         const string RedirectLodging = "RedirectLodging";
+        const string RedirectMisc = "RedirectMisc";
         const string PositiveConfirmation = "PositiveConfirmation";
         const string NegativeConfirmation = "NegativeConfirmation";
 
@@ -19,7 +20,7 @@ namespace ReloChatBot.Controllers
         private string reply;
         private Activity activity;
 
-        private bool ManualOverRide = false;
+        //private bool ManualOverRide = false;
 
         public BotController(LuisParser masterbot, Activity activity)
         {
@@ -28,13 +29,13 @@ namespace ReloChatBot.Controllers
             this.userinput = this.activity.Text;
 
             // If the Intent is a Positive or Negative Confirmation, pass it on to the last bot that was talking.
-            this.ManualOverRide = (masterbot.Intent == PositiveConfirmation || masterbot.Intent == NegativeConfirmation);
+            //this.ManualOverRide = (masterbot.Intent == PositiveConfirmation || masterbot.Intent == NegativeConfirmation);
 
-            if (masterbot.RedirectRequired || this.ManualOverRide)
+            if (masterbot.RedirectRequired)
             {
-                if (masterbot.Intent == RedirectLodging || this.GetLastBotConversation() == RedirectLodging)
+                if (masterbot.Intent == RedirectLodging)
                 {
-                    this.SetLastBotConversation(RedirectLodging);
+                    //this.SetLastBotConversation(RedirectLodging);
                     //string test1 = this.GetLastBotConversation();
                     //bool test = this.GetLastBotConversation() == RedirectLodging;
                     this.handle_RedirectLodging();
@@ -78,6 +79,11 @@ namespace ReloChatBot.Controllers
             LodgingBot lobot = new LodgingBot(this.activity);
             lobot.Seed(this.masterbot);
             this.reply = lobot.Reply;
+        }
+
+        private void handle_RedirectMiscBot()
+        {
+            this.LastBotConversation = RedirectMisc;
         }
 
         public string Reply
